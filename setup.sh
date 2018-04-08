fa87f9814d7428ecfa4b37f39cdd174e9b87b5bc
aanggoro <1511501288@student.budiluhur.ac.id>
2018-04-08 15:28:40 +0700

    rail standar enviroment

---
+++ setup.sh
@@ -0,0 +1,34 @@
+o "Provisioning virtual machine..."
+
+    echo "add depedencies"
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+    echo "update and install package for rails"
+    sudo apt-get -y update
+    sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs yarn
+
+    echo "install ruby and depedencies"
+    cd
+    wget http://ftp.ruby-lang.org/pub/ruby/2.5/ruby-2.5.0.tar.gz
+    tar -xzvf ruby-2.5.0.tar.gz
+    cd ruby-2.5.0/
+    ./configure
+    make
+    sudo make install
+    ruby -v
+
+    rbenv install 2.5.0
+    rbenv global 2.5.0
+    ruby -v
+
+    echo "rehash and install rails"
+  
+    gem install bundler
+    gem install rails
+    echo "install mysql"
+    apt-get install -y debconf-utils
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password anhar09"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password anhar09"
+    apt-get install -y mysql-server mysql-client libmysqlclient-dev	
