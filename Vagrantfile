f60b013dd97371f6de8cda8468c920556b7f7eb5
aanggoro <1511501288@student.budiluhur.ac.id>
2018-04-11 17:33:43 +0700

    replace vagrantfile, setup.sh and remove file config

--- Vagrantfile
+++ Vagrantfile
@@ -61,7 +61,7 @@ Vagrant.configure("2") do |config|
   #   vb.gui = true
   #
       # Customize the amount of memory on the VM:
-      vb.memory = "256"
+      vb.memory = "1024"
 
       # set name the provider
       vb.name = "rails test vagrantfile"
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 echo "Provisioning virtual machine..."
 
     echo "add depedencies"
-    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
